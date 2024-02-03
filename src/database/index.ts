16d3b8095eacf23de0242e676a6f4efd3b50cf8e
Felexonyango <felexonyango02@gmail.com>
2024-02-03 13:16:36 +0300

    update(added mongo url)

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,8 +3,8 @@ export const connectDb = async () => {
   try{
        
 
-    //const test="mongodb+srv://felexonyango19:Q1qRnSFLVsQ5Mb5m@cluster0.voshhwm.mongodb.net/?retryWrites=true"
-    const DB ="mongodb://127.0.0.1:27017/ProjectManagement"
+    const DB="mongodb+srv://felexonyango19:Q1qRnSFLVsQ5Mb5m@cluster0.voshhwm.mongodb.net/?retryWrites=true"
+    //const DB ="mongodb://127.0.0.1:27017/ProjectManagement"
     await mongoose.connect(DB,{
         useUnifiedTopology: true,
         useNewUrlParser: true,
