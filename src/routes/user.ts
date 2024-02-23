063f7d00013fd1ccfd99410023debbb8eacadf91
Felexonyango <felexonyango02@gmail.com>
2024-02-23 10:58:51 +0300

    updated ticket and tas routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,18 +1,141 @@
-import { Router } from 'express';
-import { deleteUser,updateUser,getAllUsers,getUserById, AssignUserRole, UnssignUserRole } from '../controller/user';
-import { authorize, protect } from "../middleware/auth"
-import { Role } from '../types';
+import { Router } from "express";
+import {
+  deleteUser,
+  updateUser,
+  getAllUsers,
+  getUserById,
+  AssignUserRole,
+  UnssignUserRole,
+} from "../controller/user";
+import { authorize, protect } from "../middleware/auth";
+import { Role } from "../types";
 
 const router = Router();
 
+/**
+ * @openapi
+ * /api/user/all:
+ *   get:
+ *     tags:
+ *       - User
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Server Error
+ */
 
-//route  admin routes\
-router.route('/all').get(protect, authorize([Role.SYSADMIN, Role.PROJECTMANAGER]),  getAllUsers);
-router.route('/:id').get(protect, authorize([Role.SYSADMIN,Role.PROJECTMANAGER]),  getUserById);
-router.route('/:id').delete(protect, authorize([Role.SYSADMIN,Role.PROJECTMANAGER]), deleteUser);
-router.route('/:id').patch(protect, authorize([Role.SYSADMIN,Role.PROJECTMANAGER]), updateUser);
-router.route("/assign/:id").patch(protect, authorize([Role.SYSADMIN,Role.PROJECTMANAGER]), AssignUserRole,UnssignUserRole);
-router.route("/unassign/:id").patch(protect, authorize([Role.SYSADMIN,Role.PROJECTMANAGER]), UnssignUserRole)
-
+router.route("/all").get(protect, authorize([Role.SYSADMIN]), getAllUsers);
+/**
+ * @openapi
+ * /api/user/{id}:
+ *   get:
+ *     tags:
+ *       - User
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Server Error
+ */
+router
+  .route("/:id")
+  .get(protect, authorize([Role.SYSADMIN, Role.USER]), getUserById);
+/**
+ * @openapi
+ * /api/user/{id}:
+ *   delete:
+ *     tags:
+ *       - User
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Server Error
+ */
+router
+  .route("/:id")
+  .delete(protect, authorize([Role.SYSADMIN, Role.USER]), deleteUser);
+/**
+ * @openapi
+ * /api/user/{id}:
+ *   patch:
+ *     tags:
+ *       - User
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Server Error
+ */
+router
+  .route("/:id")
+  .patch(protect, authorize([Role.SYSADMIN, Role.USER]), updateUser);
+/**
+ * @openapi
+ * /api/user/assign/{id}:
+ *   patch:
+ *     tags:
+ *       - User
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Server Error
+ */
+router
+  .route("/assign/:id")
+  .patch(protect, authorize([Role.SYSADMIN]), AssignUserRole, UnssignUserRole);
+/**
+ * @openapi
+ * /api/user/unassign/{id}:
+ *   patch:
+ *     tags:
+ *       - User
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Server Error
+ */
+router
+  .route("/unassign/:id")
+  .patch(protect, authorize([Role.SYSADMIN]), UnssignUserRole);
 
 export { router as userRoutes };
